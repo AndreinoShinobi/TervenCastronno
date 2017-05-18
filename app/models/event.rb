e8a19ea0b6f62f68793e2d671dd4695f1d27a632
AndreinoShinobi <andreadellerba@outlook.com>
2017-05-18 13:54:52 +0200

    event model's validation work correctly

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
-  # validates :name, presence: true
-  #
-  #
-  # validates_uniqueness_of :name, scope: :date
+  validates :name, presence: true
+  validates :date, presence: true
+
+  validates_uniqueness_of :name, scope: :date
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'create valid event' do
+    event = build(:event, :event_one)
+    assert event.save
+  end
+
+  test 'event must have name' do
+    event = build(:event, :event_without_name)
+    assert_not event.save
+  end
+
+  test 'event must have date' do
+    event = build(:event, :event_without_date)
+    assert_not event.save
+  end
+
+  test 'uniqueness of name scope date' do
+    same_date = DateTime.now
+    create(:event, name: 'same_name', date: same_date)
+
+    assert create(:event, name: 'different_name', date: same_date)
+    assert_not (build(:event, name: 'same_name', date: same_date)).save
+  end
+
 end
